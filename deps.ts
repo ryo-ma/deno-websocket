be172a614ccb21d6f38ef98a3735824daea108b5
ryo-ma <saka_ro@yahoo.co.jp>
2021-03-03 00:28:03 +0900

    Add WebSocketClient

--- deps.ts
+++ deps.ts
@@ -1,13 +1,10 @@
-export { EventEmitter } from "https://deno.land/std@0.65.0/node/events.ts";
-export { Server, serve } from "https://deno.land/std@0.65.0/http/server.ts";
+export { EventEmitter } from "https://deno.land/std@0.88.0/node/events.ts";
+export { serve, Server } from "https://deno.land/std@0.88.0/http/server.ts";
 export {
   acceptWebSocket,
   isWebSocketCloseEvent,
   isWebSocketPingEvent,
   isWebSocketPongEvent,
-  connectWebSocket,
-} from "https://deno.land/std@0.65.0/ws/mod.ts";
+} from "https://deno.land/std@0.88.0/ws/mod.ts";
 
-export type {
-  WebSocket,
-} from "https://deno.land/std@0.65.0/ws/mod.ts";
+export type { WebSocket } from "https://deno.land/std@0.88.0/ws/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,2 +1,8 @@
-export { WebSocketServer, WebSocket, WebSocketState } from "./lib/websocket.ts";
+export {
+  StandardWebSocketClient,
+  WebSocketAcceptedClient,
+  WebSocketServer,
+  WebSocketState,
+} from "./lib/websocket.ts";
+export type { WebSocketClient } from "./lib/websocket.ts";
 export { WebSocketError } from "./lib/errors.ts";
