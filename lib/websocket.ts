665cdeb43e91648fb2a5e0d2a7d27e3d884ace7f
ryo-ma <saka_ro@yahoo.co.jp>
2021-03-03 00:27:34 +0900

    Support deno v1.7.2

--- lib/websocket.ts
+++ lib/websocket.ts
@@ -1,12 +1,11 @@
 import { EventEmitter } from "./../deps.ts";
-import { Server, serve } from "./../deps.ts";
+import { serve, Server } from "./../deps.ts";
 import {
   acceptWebSocket,
   isWebSocketCloseEvent,
   isWebSocketPingEvent,
   isWebSocketPongEvent,
-  connectWebSocket,
-  WebSocket as STDWebSocket,
+  WebSocket as DenoWebSocketType,
 } from "./../deps.ts";
 
 import { WebSocketError } from "./errors.ts";
@@ -19,9 +18,12 @@ export enum WebSocketState {
 }
 
 export class WebSocketServer extends EventEmitter {
-  clients: Set<WebSocket> = new Set<WebSocket>();
+  clients: Set<WebSocketAcceptedClient> = new Set<WebSocketAcceptedClient>();
   server?: Server = undefined;
-  constructor(private port: Number = 8080, private realIpHeader: string|null = null) {
+  constructor(
+    private port: Number = 8080,
+    private realIpHeader: string | null = null,
+  ) {
     super();
     this.connect();
   }
@@ -36,15 +38,19 @@ export class WebSocketServer extends EventEmitter {
           bufWriter,
           headers,
         });
-        const ws: WebSocket = new WebSocket();
-        if(this.realIpHeader && "hostname" in sock.conn.remoteAddr) {
-          if(!req.headers.has(this.realIpHeader)) {
-            this.emit("error", new Error('specified real ip header does not exist'));
+        if (this.realIpHeader && "hostname" in sock.conn.remoteAddr) {
+          if (!req.headers.has(this.realIpHeader)) {
+            this.emit(
+              "error",
+              new Error("specified real ip header does not exist"),
+            );
           } else {
-            sock.conn.remoteAddr.hostname = req.headers.get(this.realIpHeader) || sock.conn.remoteAddr.hostname;
+            sock.conn.remoteAddr.hostname =
+              req.headers.get(this.realIpHeader) ||
+              sock.conn.remoteAddr.hostname;
           }
         }
-        ws.open(sock);
+        const ws: WebSocketAcceptedClient = new WebSocketAcceptedClient(sock);
         this.clients.add(ws);
         this.emit("connection", ws);
       } catch (err) {
@@ -59,25 +65,28 @@ export class WebSocketServer extends EventEmitter {
   }
 }
 
-export class WebSocket extends EventEmitter {
-  webSocket?: STDWebSocket;
+export interface WebSocketClient extends EventEmitter {
+  send(message: string | Uint8Array): void;
+  ping(message?: string | Uint8Array): void;
+  close(code: number, reason?: string): Promise<void>;
+  closeForce(): void;
+  isClosed: boolean | undefined;
+}
+
+export class WebSocketAcceptedClient extends EventEmitter
+  implements WebSocketClient {
   state: WebSocketState = WebSocketState.CONNECTING;
-  constructor(private endpoint?: string) {
+  webSocket: DenoWebSocketType;
+  constructor(sock: DenoWebSocketType) {
     super();
-    if (this.endpoint !== undefined) {
-      this.createSocket(endpoint);
-    }
-  }
-  async createSocket(endpoint?: string) {
-    const webSocket = await connectWebSocket(this.endpoint!);
-    this.open(webSocket);
-  }
-  async open(sock: STDWebSocket) {
     this.webSocket = sock;
+    this.open();
+  }
+  async open() {
     this.state = WebSocketState.OPEN;
     this.emit("open");
     try {
-      for await (const ev of sock) {
+      for await (const ev of this.webSocket) {
         if (typeof ev === "string") {
           // text message
           this.emit("message", ev);
@@ -101,10 +110,12 @@ export class WebSocket extends EventEmitter {
       }
     } catch (err) {
       this.emit("close", err);
-      if (!sock.isClosed) {
-        await sock.close(1000).catch((e) => {
+      if (!this.webSocket.isClosed) {
+        await this.webSocket.close(1000).catch((e) => {
           // This fixes issue #12 where if sent a null payload, the server would crash.
-          if (this.state === WebSocketState.CLOSING && sock.isClosed) {
+          if (
+            this.state === WebSocketState.CLOSING && this.webSocket.isClosed
+          ) {
             this.state = WebSocketState.CLOSED;
             return;
           }
@@ -151,5 +162,51 @@ export class WebSocket extends EventEmitter {
   }
   get isClosed(): boolean | undefined {
     return this.webSocket!.isClosed;
+  }
+}
+
+export class StandardWebSocketClient extends EventEmitter
+  implements WebSocketClient {
+  webSocket?: WebSocket;
+  constructor(private endpoint?: string) {
+    super();
+    if (this.endpoint !== undefined) {
+      this.webSocket = new WebSocket(endpoint!);
+      this.webSocket.onopen = () => this.emit("open");
+      this.webSocket.onmessage = (message) => this.emit("open", message);
+      this.webSocket.onclose = () => this.emit("close");
+      this.webSocket.onerror = () => this.emit("error");
+    }
+  }
+  async ping(message?: string | Uint8Array) {
+    if (this.webSocket?.readyState === WebSocketState.CONNECTING) {
+      throw new WebSocketError(
+        "WebSocket is not open: state 0 (CONNECTING)",
+      );
+    }
+    return this.webSocket!.send("ping");
+  }
+  async send(message: string | Uint8Array) {
+    if (this.webSocket?.readyState === WebSocketState.CONNECTING) {
+      throw new WebSocketError(
+        "WebSocket is not open: state 0 (CONNECTING)",
+      );
+    }
+    return this.webSocket!.send(message);
+  }
+  async close(code = 1000, reason?: string): Promise<void> {
+    if (
+      this.webSocket!.readyState === WebSocketState.CLOSING ||
+      this.webSocket!.readyState === WebSocketState.CLOSED
+    ) {
+      return;
+    }
+    return this.webSocket!.close(code, reason!);
+  }
+  closeForce(): void {
+    throw new Error("Method not implemented.");
+  }
+  get isClosed(): boolean | undefined {
+    return this.webSocket!.readyState === WebSocketState.CLOSED;
   }
 }
